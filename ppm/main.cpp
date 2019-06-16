798640f6e823fc047191582ef194360b0d503a79
Maximilian Kruse <maxkruse1996@gmail.com>
2019-06-16 14:15:11 +0200

    Add WxWidget Project creation via <win>

--- ppm/main.cpp
+++ ppm/main.cpp
@@ -35,7 +35,30 @@ std::string replace_all(std::string& str, const std::string& from, const std::st
 	return str;
 }
 
-void ppm_write_header(const std::string& name, const char* architecture = "x64")
+void write_file(const std::string& name, const std::string text, bool append = false, bool usePch = false)
+{
+	std::ofstream file;
+
+	if (usePch)
+	{
+		write_file(name, "#include \"pch.h\"\n");
+		append = true;
+	}
+
+	if(append)
+	{
+		file.open(name, std::fstream::app);
+	}
+	else
+	{
+		file.open(name);
+	}
+
+	file.write(text.c_str(), text.size());
+	file.close();
+}
+
+void ppm_write_header(const std::string& name, const std::string& architecture = "x64")
 {
 	std::string s = R"(workspace "__NAME__"
 	startproject "__NAME__"
@@ -51,13 +74,11 @@ void ppm_write_header(const std::string& name, const char* architecture = "x64")
 outputDir = "/%{cfg.system}/%{cfg.architecture}/%{cfg.buildcfg}")";
 	s = replace_all(s, "__NAME__", name);
 	s = replace_all(s, "__ARCHITECTURE__", architecture);
-	std::ofstream file(name + "/premake5.lua");
-	file.write(s.c_str(), s.size());
-	file.close();
 
+	write_file(name + "/premake5.lua", s);
 }
 
-void ppm_write_project(const std::string& name, const char* kind = "ConsoleApp", bool modifyExisting = false)
+void ppm_write_project(const std::string& name, const std::string& kind = "ConsoleApp", bool modifyExisting = false, const bool usePch = false, const std::string& defines = "")
 {
 	std::string s(R"(
 
@@ -73,8 +94,7 @@ project "__NAME__"
 	targetdir ("bin/%{prj.name}" .. outputDir)
 	objdir ("bin/%{prj.name}/intermediates" .. outputDir)
 
-	pchheader "pch.h"
-	pchsource "__NAME__/src/pch.cpp"
+	__PCH__
 
 	files
 	{
@@ -86,6 +106,11 @@ project "__NAME__"
 	includedirs
 	{
 		"%{prj.name}/src/include",
+	}
+
+	defines
+	{
+		"__DEFINES__"
 	}
 
 	links
@@ -103,44 +128,119 @@ project "__NAME__"
 	filter "configurations:Release"
 		optimize "On"
 		symbols "Off")");
+
+	const std::string pchStr =  usePch ? R"(pchheader "pch.h"
+	pchsource "__NAME__/src/pch.cpp")" : "";
+
+	s = replace_all(s, "__PCH__", pchStr);
 	s = replace_all(s, "__NAME__", name);
 	s = replace_all(s, "__KIND__", kind);
+	s = replace_all(s, "__DEFINES__", defines);
+	
 	std::string lua_str = modifyExisting ? ("premake5.lua") : (name + "/premake5.lua");
-	std::ofstream file(lua_str, std::fstream::app);
-	file.write(s.c_str(), s.size());
-	file.close();
+	write_file(lua_str, s, true);
 }
 
-void ppm_create_default_files(const std::string& name, bool addNameToAll = true)
+void ppm_create_default_files(const std::string& name, bool addNameToAll = true, const bool usePch = false, const std::string kind = "ConsoleApp")
 {
 	std::string main_path = addNameToAll ? (name + "/" + name) : name;
-	std::ofstream temp(main_path + "/src/main.cpp");
-	std::string s(R"(#include "pch.h"
+	if(kind == "ConsoleApp")
+	{
+		write_file(main_path + "/src/main.cpp", R"(#include "pch.h"
 
 int main(int argc, char** args)
 {
 	printf("Hello World");
 	return 0;
 }
+)", false, usePch);
+
+	}
+
+	if(kind == "WindowedApp")
+	{
+		// Main.cpp
+		write_file(main_path + "/src/Main.cpp", R"(#include <wx/wx.h>
+#include "Main.h"
+
+cMain::cMain() 
+	: wxFrame(nullptr, wxID_ANY, "Stupid Window #1", wxPoint(120, 80), wxSize(1280, 720))
+{
+	
+}
+
+cMain::~cMain() {}
+)", false, usePch);
+
+		// cApp.cpp
+		write_file(main_path + "/src/cApp.cpp", R"(#include <wx/wx.h>
+#include "Main.h"
+#include "cApp.h"
+
+wxIMPLEMENT_APP(cApp);
+
+cApp::cApp()
+{
+}
+
+cApp::~cApp()
+{
+	
+}
+
+bool cApp::OnInit()
+{
+	m_Frame = new cMain();
+	m_Frame->Show();
+	return true;
+}
+
+)", false, usePch);
+
+		// cApp.h
+		write_file(main_path + "/src/include/cApp.h", R"(#pragma once
+
+class cApp : public wxApp
+{
+public:
+	cApp();
+	~cApp();
+
+private:
+	virtual bool OnInit();
+
+	cMain* m_Frame;
+};
+
+
 )");
-	temp.write(s.c_str(), s.size());
-	temp.close();
 
-	temp.open(main_path + "/src/pch.cpp");
-	s = R"(#include "pch.h")";
-	temp.write(s.c_str(), s.size());
-	temp.close();
+		// Main.h
+		write_file(main_path + "/src/include/Main.h", R"(#pragma once
 
-	temp.open(main_path + "/src/pch.h");
-	s = R"(#include <cstdio>
+class cMain : public wxFrame
+{
+public:
+	cMain();
+	~cMain();
+private:
+};)");
+
+	}
+	
+
+	if(usePch)
+	{
+		write_file(main_path + "/src/pch.cpp", R"(#include "pch.h")");
+
+		write_file(main_path + "/src/pch.h", R"(#include <cstdio>
 #include <string>	
 #include <vector>
 #include <filesystem>
 #include <algorithm>
 #include <chrono>
-#include <utility>)";
-	temp.write(s.c_str(), s.size());
-	temp.close();
+#include <utility>)");
+	}	
 }
 
 void ppm_create_folders(const std::string& name, bool addNameToAll = true)
@@ -160,7 +260,7 @@ void ppm_create_folders(const std::string& name, bool addNameToAll = true)
 	}
 }
 
-void ppm_append_project(const char* name, const char* kind = "ConsoleApp", const char* architecture = "x64")
+void ppm_append_project(const std::string& name, const std::string& kind = "ConsoleApp", const bool usePch = false)
 {
 	if (!std::filesystem::exists("./premake5.lua"))
 	{
@@ -168,24 +268,41 @@ void ppm_append_project(const char* name, const char* kind = "ConsoleApp", const
 		return;
 	}
 	ppm_create_folders(name, false);
-	ppm_write_project(name, kind, true);
+	ppm_write_project(name, kind, true, usePch);
 
-	if(!strcmp(kind, "ConsoleApp"))
-	{
-		ppm_create_default_files(name, false);
-	}
+	
+	ppm_create_default_files(name, false, usePch, kind);
+	
 }
 
-void ppm_init_project(const char* name, const char* kind = "ConsoleApp", const char* architecture = "x64")
+void ppm_init_project(const std::string& name, const std::string& kind = "ConsoleApp", const bool usePch = false)
 {
 	ppm_create_folders(name);
 	ppm_write_header(name);
-	ppm_write_project(name, kind);
 
-	if (!strcmp(kind, "ConsoleApp"))
-	{
-		ppm_create_default_files(name);
-	}
+	const std::string defineStr = kind == "WindowedApp" ? "WXUSINGDLL" : "";
+
+	ppm_write_project(name, kind, false, usePch, defineStr);
+
+	
+	ppm_create_default_files(name, true, usePch, kind);
+	
+}
+
+void print_help()
+{
+	printf("\t%-*s", 20, "<app>"); 
+	printf("%s\n", "Console Application");
+
+	printf("\t%-*s", 20, "<lib>");
+	printf("%s\n", "Static Library");
+
+	printf("\t%-*s", 20, "<dll>");
+	printf("%s\n", "Dynamic Library");
+
+	printf("\t%-*s", 20, "<win>");
+	printf("%s\n", "Window Application, using WxWidget (install with vcpkg: \"vcpkg install wxwidget:x64-windows\", for further information check the vcpkg help page)");
+
 }
 
 int main(const int argc, char** args)
@@ -193,14 +310,23 @@ int main(const int argc, char** args)
 	auto cmdParser = new CP::CommandParser(argc, args);
 
 	cmdParser->RegisterCommand({ "-v", "Version", "Print the version" });
-	cmdParser->RegisterCommand({ "version", "Version", "Print the version" });
 	cmdParser->RegisterCommand({ "-version", "Version", "Print the version" });
+	cmdParser->RegisterCommand({ "-help", "Help", "Print help" });
+	cmdParser->RegisterCommand({ "-pch", "PCH", "Use pch" });
 	cmdParser->ConsumeFlags();
+	const bool usePch = cmdParser->HasCommand("PCH");
 
 	if (cmdParser->HasCommand("Version"))
 	{
 		printf("%s, v%s\n", APP_NAME, APP_VERSION);
 		printf("Build Info: %s", BUILD_INFO);
+		return 0;
+	}
+
+	if(cmdParser->HasCommand("Help"))
+	{
+		cmdParser->PrintUsage({ "init/add", "app/lib/dll/win", "name" });
+		print_help();
 		return 0;
 	}
 
@@ -213,25 +339,23 @@ int main(const int argc, char** args)
 	if (cmdParser->GetParam(1) == "init")
 	{
 		const auto type = cmdParser->GetParam(2);
-		const auto name = cmdParser->GetParam(3).c_str();
-		ppm_init_project(name);
-
+		const auto name = cmdParser->GetParam(3);
 
 		if (type == "app")
 		{
-			ppm_init_project(name);
+			ppm_init_project(name, "ConsoleApp", usePch);
 		}
 		else if (type == "lib")
 		{
-			ppm_init_project(name, "StaticLib");
+			ppm_init_project(name, "StaticLib", usePch);
 		}
 		else if (type == "dll")
 		{
-			ppm_init_project(name, "SharedLib");
+			ppm_init_project(name, "SharedLib", usePch);
 		}
 		else if (type == "win")
 		{
-			ppm_init_project(name, "WindowedApp");
+			ppm_init_project(name, "WindowedApp", usePch);
 		}
 		else
 		{
@@ -243,8 +367,7 @@ int main(const int argc, char** args)
 	else if (cmdParser->GetParam(1) == "add")
 	{
 		const auto type = cmdParser->GetParam(2);
-		const auto name = cmdParser->GetParam(3).c_str();
-		ppm_init_project(name);
+		const auto name = cmdParser->GetParam(3);
 
 
 		if (type == "app")
@@ -253,15 +376,15 @@ int main(const int argc, char** args)
 		}
 		else if (type == "lib")
 		{
-			ppm_append_project(name, "StaticLib");
+			ppm_append_project(name, "StaticLib", usePch);
 		}
 		else if (type == "dll")
 		{
-			ppm_append_project(name, "SharedLib");
+			ppm_append_project(name, "SharedLib", usePch);
 		}
 		else if (type == "win")
 		{
-			ppm_append_project(name, "WindowedApp");
+			ppm_append_project(name, "WindowedApp", usePch);
 		}
 		else
 		{
